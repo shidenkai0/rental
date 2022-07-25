d068f62d9fef81e08f8a2536041e7b89e374aba6
Mohamed Messaad <hajimenomomo@gmail.com>
2022-07-25 19:09:42 +0200

    feat: add deployment script.

---
+++ deployment/bin/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script is meant to be called from the project Makefile.
+
+cd deployment/helm/api || exit
+
+if [ -n "$CI" ]; then
+    if [ -z "$VALUES_PROD_BASE64" ]; then
+        echo "VALUES_PROD_BASE64 is not set. Exiting."
+        exit 1
+    fi
+    echo "${VALUES_PROD_BASE64}" | base64 --decode > /tmp/values-prod.yaml
+	helm upgrade api --install -f /tmp/values-prod.yaml \
+	 --set image.tag="${COMMIT_SHA}" --set image.repository="${DOCKER_REGISTRY}/api" \
+	 . || true # Continue to make sure we remove values-prod.yaml even if the helm command fails.
+    rm /tmp/values-prod.yaml
+else
+	helm upgrade api --install -f values-prod.yaml \
+	 --set image.tag="${COMMIT_SHA}" --set image.repository="${DOCKER_REGISTRY}/api" \
+	 .
+fi
